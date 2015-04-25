4c249920836c9b7fa76b954a0099413d8954e5a9
Frédéric Langlade-Bellone <fred@parapluie.org>
2015-04-25 22:38:57 +0200

    eslint integration

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Model from "src/model.js";
 import Collection from "src/collection.js";
 
-export Model;
-export Collection;
+export const Model = Model;
+export const Collection = Collection;
